634c50aee90c90861adf069ca1c81cc7d565ede5
Zakaria Farahi <zakariafarahi521@gmail.com>
2026-01-11 13:08:21 +0100

    react-app changes

--- react-app/src/App.test.js
+++ react-app/src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock("axios", () => ({
+  get: jest.fn(),
+  post: jest.fn(),
+}));
+
+test("renders app header", () => {
+  const keycloak = {
+    idTokenParsed: {
+      preferred_username: "admin1",
+      email: "admin1@microservices.com",
+    },
+    tokenParsed: {
+      realm_access: {
+        roles: ["MS_ADMIN"],
+      },
+    },
+    logout: jest.fn(),
+  };
+
+  render(<App keycloak={keycloak} />);
+  const header = screen.getByText(/React \+ Keycloak \+ API Gateway/i);
+  expect(header).toBeInTheDocument();
 });
